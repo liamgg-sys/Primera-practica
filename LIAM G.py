c1c5157a36e58a746bde705be188e53fae0fa8ec
liamgg-sys <liamgg@isb.edu.mx>
2025-09-19 14:52:49 +0000

    practica cumple

---
+++ LIAM G.py
@@ -0,0 +1,18 @@
+import time
+from datetime import datetime
+
+print("biemvenido👽")
+
+time.sleep(1)
+
+
+
+nombre=input("ingresa tu nombre:")
+edad=int(input("ingresa tu edad:"))
+pasatiempo=input("ingresa tu pasatiempo:")
+materia=intput("ingresa tu materia:")
+
+
+
+print("ahora vamos a calcular cuantos dias faltan para tu cumple🎂")
+
